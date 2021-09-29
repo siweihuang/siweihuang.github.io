3950a0f06bbe8471647bd62236b70ab06f85c64a
siweihuang <siwei.io.huang@gmail.com>
2021-09-29 20:55:04 +0800

    Added Education module for ChinesePinyin HTML page

---
+++ Education/ChinesePinyinHTML/Tingting_Data.js
@@ -0,0 +1,42 @@
+var mainData = [
+    {
+        "title": "静夜思",
+        "subtitle": "(唐)李白",
+        "paragraph": [
+            { "bodytext": "床前明月光，疑是地上霜。" },
+            { "bodytext": "举头望明月，低头思故乡。" }
+        ]
+    },
+    {
+        "title": "相思",
+        "subtitle": "(唐)王维",
+        "paragraph": [
+            { "bodytext": "红豆生南国，春来发几枝？" },
+            { "bodytext": "愿君多采撷，此物最相思。" }
+        ]
+    },
+    {
+        "title": "夜宿山寺",
+        "subtitle": "(唐)李白",
+        "paragraph": [
+            { "bodytext": "危楼高百尺，手可摘星辰。" },
+            { "bodytext": "不敢高声语，恐惊天上人。" }
+        ]
+    },
+    {
+        "title": "梅花",
+        "subtitle": "(唐)王安石",
+        "paragraph": [
+            { "bodytext": "墙角数枝梅，凌寒独自开。" },
+            { "bodytext": "遥知不是雪，为有暗香来。" },
+        ]
+    },
+    {
+        "title": "所见",
+        "subtitle": "(清)袁枚",
+        "paragraph": [
+            { "bodytext": "牧童骑黄牛，歌声振林樾。" },
+            { "bodytext": "意欲捕鸣蝉，忽然闭口立。" },
+        ]
+    }
+]
