833473d7686a4f1410f0f57a4f58ffdef3ada9d4
siweihuang <siwei.io.huang@gmail.com>
2022-09-15 19:05:06 +0800

    Added the new Chinse Spelling and English Spelling module under Education. Enjoy.

---
+++ Education/DataSource/MasterEnglishWords.js
@@ -0,0 +1,32 @@
+var MasterEnglishWords = {
+    "greet":  {
+        "word":"greet",
+        "explanation":"to welcome someone with particular words or a particular action, or to react to something in the stated way",
+        "audio":"https://dictionary.cambridge.org/media/english/uk_pron/u/ukg/ukgre/ukgreen018.mp3",
+        "category":"verb"
+    },
+    "prepare":  {
+        "word":"prepare",
+        "explanation":"to make or get something or someone ready for something that will happen in the future",
+        "audio":"https://dictionary.cambridge.org/media/english/uk_pron/u/ukp/ukpre/ukpremi021.mp3",
+        "category":"verb"
+    },
+    "celebrate":  {
+        "word":"celebrate",
+        "explanation":"to take part in special enjoyable activities in order to show that a particular occasion is important",
+        "audio":"https://dictionary.cambridge.org/media/english/uk_pron/u/ukc/ukcel/ukceleb001.mp3",
+        "category":"verb"
+    },
+    "vegetables":  {
+        "word":"vegetables",
+        "explanation":"",
+        "audio":"https://dictionary.cambridge.org/media/english/uk_pron/u/ukv/ukvec/ukvecto010.mp3",
+        "category":"noun"
+    },
+    "castle": {
+        "word":"castle",
+        "explanation":"a large strong building, built in the past by a ruler or important person to protect the people inside from attack",
+        "audio":"https://dictionary.cambridge.org/media/english/uk_pron/u/ukc/ukcas/ukcaste003.mp3",
+        "category":"noun"
+    }
+}
---
+++ Education/DataSource/englishspelling.js
@@ -0,0 +1,20 @@
+var mainData = [
+    {
+        "title": "Unit 11 - The First Day of Hari Raya",
+        "subtitle":"Unit 11",
+        "testdate":"19/09/2022",
+        "words": ["greet","prepare","celebrate","wait","playful","sorry","first","gifts","packets","kitchen"]
+    },
+    {
+        "title": "Unit 10 - Mid-Autum Festival",
+        "subtitle":"Unit 10",
+        "testdate":"11/08/2022",
+        "words": ["festival","lanterns","stall","stool","street","price","dumplings","meat","mooncakes","vegetables"]
+    },
+    {
+        "title": "Unit 9 - When My Baby Sister Came Home",
+        "subtitle":"Unit 9",
+        "testdate":"",
+        "words": ["sister","siblings","cousin","grandparents","diary","often","soften","listen","castle","carried"]
+    }
+]
