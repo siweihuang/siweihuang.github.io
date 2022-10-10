0f6d38accf59ed9c71f7081166a69dc0b4065055
siweihuang <siwei.io.huang@gmail.com>
2022-10-10 23:22:54 +0800

    Added English Comprehension module

---
+++ Education/DataSource/englishcomprehension.js
@@ -0,0 +1,17 @@
+var mainData = [
+    {
+        "title": "Reading Comprehension (7 - 12)",
+        "subtitle":"Practices (7 - 12)",
+        "testdate":"",
+        "words": ["The weather was very hot.",
+        "Anne left her bedroom because she wanted to get a blanket from her parents' room.",
+        "The goose could lay a golden egg every day.",
+        "The couple had to beg in the streets when they became poor.",
+        "They wanted to be rich more quickly.",
+        "Based on the story, we can tell that the couple were greedy people.",
+        "Mulan's father did not exepect her to be a better fighter than him.",
+        "Why did Ali rub the lamp?",
+        "The lamp was filthy so he wanted to rub it clean.",
+        "Ali learnt not to be lazy."]
+    }
+]
