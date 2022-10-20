5745a0d935ee444f82e719995f1eac93cca727ea
siweihuang <siwei.io.huang@gmail.com>
2022-10-20 23:23:15 +0800

    Added new module Songs

---
+++ Education/DataSource/songs.js
@@ -0,0 +1,25 @@
+var mainData = [
+    {
+        "title": "无条件 - Eason Chan",
+        "subtitle":"无条件",
+        "testdate":"10/07/2015",
+        "words": [
+            "你 何以始终不说话 尽管讲出不快吧<br>You, why still haven’t spoken a word.<br>Say all the things that upset you.",
+            "事与冀盼有落差 请不必惊怕<br>When things differ to what you expected, please don’t be scared.",
+            "我 仍然会冷静聆听 仍然紧守于身边<br>与你进退也共鸣<br>I, will still be calmly listening. Will stay next to you, move with you with resonance.",
+            "时日会蔓延再蔓延 某些不可改变的改变<br>与一些不要发现的发现 就这么放大了缺点<br>Time will extend and extend, something that will never be able to change will change and those that shouldn’t be discovered will be discovered, and so the negative side will be enlarged.",
+            "来让我问谁可决定<br>那些东西叫作完美至善<br>Let me ask who is the one that could determine what is called perfect.",
+            "我只懂得 爱你在每天<br>All I know is to love you every day.",
+            "当潮流爱新鲜 当旁人爱标签 幸得伴著你我 是窝心的自然<br>When the trend is to love the new things; when people around love to label others, lucky that accompanying you and me is the touching normality.",
+            "当闲言再尖酸 给他妒忌多点 因世上的至爱<br>是不计较条件 谁又可清楚看见<br>When words get more nasty, let him get more jealous, because the true love in this world is unconditional, who could see this clearly.",
+            "美 难免总有些缺憾 若果不甘心去问<br>问到最尾叫内心 也长出裂痕<br>Beauty, is hard not to have any shortcoming; if you are not willing to ask, when you got to the end, a crack will grow in your heart.",
+            "笑 何妨与你又重温 仍然我说我庆幸 你永远胜过别人<br>Laugh, why not revisit them with you. I still say that I am lucky, as you are always better than others.",
+            "期待美没完爱没完<br>放开不必打算的打算<br>Looking forward to unlimited beauty and love, let go of those plan of unnecessary plan.",
+            "作一些可以约定的约定<br>就抱紧以后每一天<br>Make some promise that can be made, and get hold of every day in the future.",
+            "其实你定然都发现<br>我有很多未达完美事情<br>Really, you must have realized that I got many things that haven’t be prefect.",
+            "我只懂得 再努力每天<br>All I know is to work hard every day.",
+            "当潮流爱新鲜 当旁人爱标签 幸得伴著你我 是窝心的自然<br>When the trend is to love the new things; when people around love to label others, lucky that accompanying you and me is the touching normality.",
+            "当闲言再尖酸 给他妒忌多点 因世上的至爱 是不计较条件 谁又可清楚看见<br>When words get more nasty, let him get more jealous, because the true love in this world is unconditional, who could see this clearly."
+        ]
+    }
+]
