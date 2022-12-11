f16b789788943357cfc03335b1eca2f77a0497e1
siweihuang <siwei.io.huang@gmail.com>
2022-12-11 12:40:33 +0800

    Updated englishcomprehension.js

--- Education/DataSource/englishcomprehension.js
+++ Education/DataSource/englishcomprehension.js
@@ -13,5 +13,28 @@ var mainData = [
         "Why did Ali rub the lamp?",
         "The lamp was filthy so he wanted to rub it clean.",
         "Ali learnt not to be lazy."]
+    },
+    {
+        "title": "Reading Comprehension (13 - 20)",
+        "subtitle":"Practices (13 - 20)",
+        "testdate":"",
+        "words": [
+            "It does not cost <b><u>much</u></b> money to eat at the canteen.",
+            "He had <b><u>many</u></b> coins inside his wallet.",
+            "There was very <b><u>little</u></b> food left by the time I arrived.",
+            "She was given a <b><u>few</u></b> strawberries as a snack.",
+            "Do you know how <b><u>many</u></b> people turned up at school today?",
+            "There are <b><u>many</u></b> cars in Singapore.",
+            '"How <b><u>much</u></b> sugar should I add to your coffee?" I asked my aunt.',
+            "I received <b><u>many</u></b> birthday presents from my relatives and friends.",
+            'Gerald whined, "I have so <b><u>much</u></b> homework today!"',
+            "Hurry! We are going to be late! We do not have <b><u>much</u></b> time left.",
+            "Andrew bought <b><u>a few</u></b> packets of apple juice for the picnic tomorrow.",
+            "Please spread <b><u>a little</u></b> butter on the bread before toasting it.",
+            "I just need <b><u>a few</u></b> more minutes to get ready! Wait for me, please!",
+            "Avery added <b><u>a few</u></b> drops of food colouring to the cookied dough.",
+            '"May I have <b><u>a little</u></b> water, please?" Anna asked.',
+            "People show <b><u>a little</u></b> pride in your work and wrtie neatly!"
+        ]
     }
 ]
