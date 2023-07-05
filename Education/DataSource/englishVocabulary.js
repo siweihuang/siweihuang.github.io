fca8cf1b7354b772444a01fbf7a2735effb39047
siweihuang <siwei.io.huang@gmail.com>
2023-07-05 10:02:39 +0800

    Added Chinese Web Input, English Vocabulary

---
+++ Education/DataSource/englishVocabulary.js
@@ -0,0 +1,29 @@
+var mainData = [
+    {
+        "title": "File 1 - Food And Cooking",
+        "subtitle":"File 1 - Food And Cooking",
+        "testdate":"01/07/2023",
+        "words": [
+            "aubergine",
+            "baked",
+            "beans",
+            "beef",
+            "beetroot",
+            "boiled",
+            "cabbage",
+            "cherries",
+            "chicken",
+            "courgette",
+            "crab",
+            "cucumber",
+            "duck",
+            "egg",
+            "fish",
+            "fried",
+            "fruit"],
+        "sentences": [
+            "You need an <b><u>aubergine</u></b> for this pasta dish.",
+            "I had a <b><u>baked</u></b> potato with tuna for my lunch."
+            ],
+    }
+]
