b732f376b5bbc97ee167b440948792d9f6739bfb
siweihuang <siwei.io.huang@gmail.com>
2023-07-31 00:47:53 +0800

    Added English Phonics

---
+++ Education/DataSource/englishPhonicsCategory.js
@@ -0,0 +1,38 @@
+var mainDataCategory = [
+    {
+        "title": "长元音",
+        "subtitle":"Long Vowels"
+    },
+    {
+        "title": "短元音",
+        "subtitle":"Short Vowels"
+    },
+    {
+        "title": "双元音",
+        "subtitle":"Diphthong Vowels"
+    },
+    {
+        "title": "爆破音",
+        "subtitle":"Plosive"
+    },
+    {
+        "title": "摩擦音",
+        "subtitle":"Fricatives"
+    },
+    {
+        "title": "破擦音",
+        "subtitle":"Affricates"
+    },
+    {
+        "title": "鼻音",
+        "subtitle":"Nasal Sounds"
+    },
+    {
+        "title": "舌则音",
+        "subtitle":"Tongue Sounds"
+    },
+    {
+        "title": "半元音",
+        "subtitle":"Semivowel"
+    }
+]
