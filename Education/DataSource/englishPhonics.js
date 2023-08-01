175c05541e1a4791d81521e2fff984c3677eec62
siweihuang <siwei.io.huang@gmail.com>
2023-08-02 02:49:22 +0800

    Updated  /ɪ/ and  /aɪ/

--- Education/DataSource/englishPhonics.js
+++ Education/DataSource/englishPhonics.js
@@ -18,8 +18,8 @@ var mainData = [
                 "h<b>e</b>","sh<b>e</b>","m<b>e</b>","w<b>e</b>","b<b>e</b>","<b>e</b>ve",
                 "sh<b>ee</b>p","gr<b>ee</b>n","f<b>ee</b>","tr<b>ee</b>","t<b>ee</b>th","s<b>ee</b>","s<b>ea</b>t","st<b>ea</b>m","m<b>ea</b>n","s<b>ea</b>","fl<b>ea</b>","t<b>ea</b>","ach<b>ie</b>ve","bel<b>ie</b>ve","bel<b>ie</b>f","rel<b>ie</b>ve","rec<b>ei</b>ve","conc<b>ei</b>t","dec<b>ei</b>t","conc<b>ei</b>ve"
             ],
-            [],
-            [],
+            ["s<b>i</b>t","f<b>i</b>sh","k<b>i</b>ng","w<b>i</b>n","l<b>i</b>ve","h<b>i</b>t","l<b>i</b>ttle","b<b>i</b>t","k<b>i</b>ss","l<b>i</b>st","sh<b>i</b>p","k<b>i</b>ck","f<b>i</b>ll","m<b>i</b>rror","ch<b>i</b>ck<b>e</b>n","p<b>i</b>t<b>y</b>","w<b>i</b>sh","off<b>i</b>ce","l<b>i</b>l<b>y</b>","rh<b>y</b>thm","bic<b>y</b>cle","famil<b>y</b>","slowl<b>y</b>","r<b>e</b>main","t<b>i</b>ck<b>e</b>t","d<b>e</b>lay","<b>e</b>conom<b>y</b>","<b>e</b>scape","<b>e</b>xample","w<b>o</b>m<b>e</b>n"],
+            ["wr<b>i</b>te","f<b>igh</b>t","wh<b>y</b>","m<b>y</b>","satisf<b>y</b>","l<b>i</b>on","t<b>i</b>me","r<b>i</b>ce","t<b>i</b>ger","fl<b>y</b>","cr<b>y</b>","sh<b>y</b>","l<b>igh</b>t","<b>ais</b>le","d<b>ye</b>"],
             [],
             [],
             [],
@@ -53,8 +53,8 @@ var mainData = [
                 "/dɪ'siːt/ n. 欺骗",
                 "/kən'siːv/ vt. 构思"
             ],
-            [],
-            [],
+            ["/sɪt/ vi. 坐","/fɪʃ/ n. 鱼","/kɪŋ/ n. 国王","/wɪn/ vi. 赢","/lɪv/ vi. 居住","/hɪt/ vt. 打击","/lɪtl/ adj. 很少的","/bɪt/ adj. 很小的","/kɪs/ vi. 接吻","/lɪst/ n. 目录","/ʃɪp/ n. 船","/kɪk/ vt. 踢","/fɪl/ vt. 装满","/'mɪrə/ n. 镜子","/'tʃɪkɪn/ n. 鸡肉","/'pɪtɪ/ n. 怜悯","/wɪʃ/ n. 希望","/'ɒfɪs/ n. 办公室","/'lɪlɪ/ n. 百合花","/'rɪðəm/ n. 节奏","/'baɪsɪkl/ n. 自行车","/ˈfæməlɪ/ n. 家庭","/'sləʊlɪ/ adv. 缓慢地","/rɪ'meɪn/ vi.剩余","/'tɪkɪt/ n. 票","/dɪ'leɪ/ vi. 延期","/ɪ'kɒnəmɪ/ n. 经济","/ɪ'skeɪp/ vt. 逃避","/ɪg'zɑːmpəl/ n. 例子","/'wɪmɪn/ n. 女人"],
+            ["/raɪt/ vi. 写","/faɪt/ vi. 打架","/waɪ/ adv. 为什么","/maɪ/ pron. 我的","/'sætɪsfaɪ/ vt. 满足","/'laɪən/ n. 狮子","/taɪm/ n. 时间","/raɪs/ n.米饭","/'taɪgə/ n. 老虎","/flaɪ/ vi. 飞","/kraɪ/ vi. 哭","/ʃaɪ/ adj. 害羞的","/laɪt/ n. 光","/aɪl/ n. 通道","/daɪ/ n. 染料"],
             [],
             [],
             [],
