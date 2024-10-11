3090344405908ea64a15b37c245501a8ec704b7d
siweihuang <siwei.io.huang@gmail.com>
2024-10-12 01:13:43 +0800

    Updated GL_Data

--- Education/Buddhism/data/gl_data_index.js
+++ Education/Buddhism/data/gl_data_index.js
@@ -2,14 +2,23 @@ var mainData = [
   {
     title: "听闻 - 听闻轨理",
     subtitle: "",
+    testdate: "14/07/2024",
     category: "file",
     filename: "gl_data_001",
     foldername: ""
   },
   {
+    title: "学习静虑波罗蜜多分五",
+    subtitle: "",
+    testdate: "11/10/2024",
+    category: "file",
+    filename: "gl_data_321_L12_323_L2"
+  },
+  {
     title: "学习般若波罗蜜多分五",
     subtitle: "",
+    testdate: "11/10/2024",
     category: "file",
-    filename: "gl_data_323_L4_L7"
+    filename: "gl_data_323_L3_324_L13"
   }
 ];
