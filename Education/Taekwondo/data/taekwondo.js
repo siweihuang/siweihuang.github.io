d5782f06956ad05512bd2d6e042b00ec020fab1e
siweihuang <siwei.io.huang@gmail.com>
2025-01-02 00:05:26 +0800

    Updated Taekwondo section

---
+++ Education/Taekwondo/data/taekwondo.js
@@ -0,0 +1,119 @@
+var mainData = [
+  {
+    belt_color: "White Belt (W10)",
+    pattern: "I Pattern",
+    meaning: "",
+    rank: "10th Geup",
+    level: "Beginner",
+    video: "",
+    category: "file",
+    filename: "Taegeuk_5_O_Jang",
+    foldername: ""
+  },
+  {
+    belt_color: "Yellow Tip (W9)",
+    pattern: "",
+    meaning: "",
+    rank: "9th Geup",
+    level: "Beginner",
+    video: "",
+    category: "file",
+    filename: "Taegeuk_5_O_Jang",
+    foldername: ""
+  },
+  {
+    belt_color: "Yellow Belt (Y8)",
+    pattern: "Taegeuk 1<br>Il Jang",
+    meaning: "太极第一章 乾 (天)<br>Heaven & Light",
+    rank: "8th Geup",
+    level: "Beginner",
+    video:
+      "https://www.youtube.com/watch?v=WvnQmtjBmo8&pp=ygUSdGFlZ2V1ayAxIGlsIGphbmcg",
+    category: "file",
+    filename: "Taegeuk_5_O_Jang",
+    foldername: ""
+  },
+  {
+    belt_color: "Green Tip (Y7)",
+    pattern: "Taegeuk 2<br>Ee Jang",
+    meaning: "太极第二章 兑 (泽)<br>Joyfulness",
+    rank: "7th Geup",
+    level: "Beginner",
+    video: "https://www.youtube.com/watch?v=u_-gfpYK5NQ",
+    category: "file",
+    filename: "Taegeuk_5_O_Jang",
+    foldername: ""
+  },
+  {
+    belt_color: "Green Belt (G6)",
+    pattern: "Taegeuk 3<br>Sam Jang",
+    meaning: "太极第三章 离 (火)<br>Fire",
+    rank: "6th Geup",
+    level: "Intermediate",
+    video: "https://www.youtube.com/watch?v=FggbmUaZlkA",
+    category: "file",
+    filename: "Taegeuk_5_O_Jang",
+    foldername: ""
+  },
+  {
+    belt_color: "Blue Tip (G5)",
+    pattern: "Taegeuk 4<br>Sar Jang",
+    meaning: "太极第四章 震 (雷)<br>Thunder",
+    rank: "5th Geup",
+    level: "Intermediate",
+    video: "https://www.youtube.com/watch?v=Pg-qye6A7fA",
+    category: "file",
+    filename: "Taegeuk_5_O_Jang",
+    foldername: ""
+  },
+  {
+    belt_color: "Blue Belt (B4)",
+    pattern: "Taegeuk 5<br>O Jang",
+    meaning: "太极第五章 巽 (风)<br>Wind",
+    rank: "4th Geup",
+    level: "Intermediate",
+    video: "https://www.youtube.com/watch?v=965K1JjpAVA",
+    category: "file",
+    filename: "Taegeuk_5_O_Jang",
+    foldername: ""
+  },
+  {
+    belt_color: "Red Tip (B3)",
+    pattern: "Taegeuk 6<br>Yook Jang",
+    meaning: "太极第六章 坎 (水)<br>Water",
+    rank: "3rd Geup",
+    level: "Advanced",
+    video: "https://www.youtube.com/watch?v=YJMfV1HY398",
+    category: "file",
+    filename: "Taegeuk_5_O_Jang",
+    foldername: ""
+  },
+  {
+    belt_color: "Red Belt (R2)",
+    pattern: "Taegeuk 7<br>Chil Jang",
+    meaning: "太极第七章 艮 (山)<br>Mountain",
+    rank: "2nd Geup",
+    level: "Advanced",
+    video: "https://www.youtube.com/watch?v=POfoHAkhwlQ&t=1s",
+    category: "file",
+    filename: "Taegeuk_5_O_Jang",
+    foldername: ""
+  },
+  {
+    belt_color: "Black tip (R1)",
+    pattern: "Taegeuk 8<br>Pal Jang",
+    meaning: "太极第八章 乾 (大地)<br>Earth",
+    rank: "1st Geup",
+    level: "Advanced",
+    video:
+      "https://www.youtube.com/watch?v=zPSt3azIo48&pp=ygUSdGFlZ2V1ayA4IHBhbCBqYW5n"
+  },
+  {
+    belt_color: "Black belt",
+    pattern: "",
+    meaning: "",
+    rank: "1st Dan",
+    level: "Beginner black belt",
+    video: ""
+  }
+];
