5a8472b7f6af7046d435a21df544a9cc9f08862a
siweihuang <siwei.io.huang@gmail.com>
2025-02-11 01:12:32 +0800

    Added English Spelling 2025 Unit 3

--- Education/DataSource/readingnotes.js
+++ Education/DataSource/readingnotes.js
@@ -1,68 +1,68 @@
 var mainData = [
-    {
-        "title": "365 Part 1",
-        "subtitle":"365",
-        "testdate":"29/09/2022",
-        "words": [
-            "A better understanding of culture and humanity through out the world",
-            "We can change our lives. We can do, have, and be exactly what we wish.",
-            "You will not know how far you can go",
-            "Our bodies are our gardens.<br>Our wills are gardeners. <br> -- William Sharespeare",
-            "Fear comes from the selfish idea of cutting one's self off from the universe",
-            "Live your own life. That is to say, where you are, as you are, with what you are, and with who you are. Accept the situation in which you find yourself and try, at the same time, to adapt to it. You cannot escape from it.",
-            "It is only when we give complete attention to a problem, and solve it immediately - never carrying it over to the next day, the next minute, that there is solitude.",
-            "A mind that is burdened with the past is a sorrowful mind.",
-            "The known are our house, our furniture, our family, our character, our work, our knowledge, our fame, our loneliness, our gods...",
-            "There is no master, there is no instructor, there is no person to tell you what you must do.",
-            "When you use the word 'how', you really want someone to tell you what to do, some guide, some system, someone to lead you by the hand so that you lose your freedom, your capacity to observe, your own activities, your own thoughts, your own way of life.",
-            "You cannot expect anything, because the expectation is within you, and what you are waiting for is dependent on external forces.",
-            "We should never try to follow another's path for that is his way, not yours. Your way is the best for you, but that is no sign it is the best for another.",
-            "We do not have the right to force others to see the truth in our way."
-        ]
-    },
-    {
-        "title": "365 Part 2",
-        "subtitle":"365",
-        "testdate":"23/10/2022",
-        "words": [
-            "The person who can wear the mantel of a Master is one who is capable of solving his own emotional and mental problems and who can show others the way to overcome their emotional and mental problems.",
-            "To go from opinion to perception, from imagination to fact, from illusion to reality, from something that is not there, to something that is; that is the way forward.",
-            "To grow is to go beyond what you are today. Stand up at yourself. Do not imitate. Do not pretend to have achieved your goal and do not try to cut corner. Just try to grow.",
-            "The human soul travels from law to love, from discipline to freedom, from the moral plane to the spiritual plane.",
-            "Wise people, who know and understand the soul, are indifferent to both pleasure and pain; they have risen above sensations. They are indifferent to the past and the future; they have risen above time. They are indifferent to danger; they have risen above fear. Wise people know that what is here, is also there; that what was, will also be. They see unity, not division.",
-            "The outward freedom that we shall attain will only be in exact proportion to the inward freedom to which we may have grown at any given moment.",
-            "Very few people in this world can reason normally. There is a terrible tendency to accept all that is said, all that is read, and to accept it without question. Only he who is ready to question, to think for himself, will find the truth! <br>To understand the current of a river, he who wishes to know the truth must enter the river.",
-            "By the term religion, I am using it in its broadest sense, meaning thereby self realization or knowledge of the self.",
-            "Allow your son to say foolish things. They are foolish things from your point of view, but when he speaks, agree with him. Say, 'all right, yes. I understand what you mean. Yes, I agree.' Once he has expressed his opinion, then try to show him that there are inconsistencies in some of what he is saying.",
-            "Guard your tongue, for it is highly dangerous; unguarded words can cause terrible distress. A single bad word can destroy a vast quantity of good. A wound caused by fire will eventually heal; but a wound caused by the tongue leaves a scar that never heals.",
-            "Watching and listening are a great art. By watching and listening we learn infinitely more than we do from any books. Books are necessary, but watching and listening sharpen your senses.",
-            "The healing of the mind takes place gradually on contact with nature, with the orange on the branch, the blade of grass eating its way into the cement, and the hills hidden by the clouds."
-        ]
-    },
-    {
-        "title": "365 Part 3",
-        "subtitle":"365",
-        "testdate":"29/10/2022",
-        "words": [
-            "How can we be free to look and learn when our minds from the moment we are born to the moment we die are shaped by a particular culture in the narrow pattern of 'me?'",
-            "For centuries, we have been conditioned by nationality, castle, class, tradition, religion, language, education, literature, art, custom, convention, propaganda of all kinds, economic pressure, the food we eat, the climate we live in, our family, our friends, our experience - every influence you can think of - and therefore our responses to every problem are conditioned.",
-            "Are you aware that you are conditioned?",
-            "Sensibility is the capacity to feel, recognize, and distinguish the most tiny and subtle changes.",
-            "No matter how insignificant the thing you have to do, do it as well as you can, give it as much of your care and attention as you would give to the thing you regard as most important.",
-            "The intellectual aspect is, that love sees and understands. The emotional aspect is to feel as one with the other person. Love is unity. There is no 'me' in love, only 'you'. The behavioral aspect is, that love inspires us to give. There is no expectation; we do not expect to receive. Such love is wisdom nad liberation in itself."
-        ]
-    },
-    {
-        "title": "Management Part 1",
-        "subtitle":"Management",
-        "testdate":"27/10/2022",
-        "words": [
-            "So the best you can do with someone who is constantly complaining, and for everyone around him, is work with him to diminish his feelings of helplessness by helping him to identify solutions.",
-            "a) Listen for main points <br>b) Interrupt and get specific <br>c) Shift the focus to solutions",
-            "- What do you want? <br>~ I don't know <br>- Guess, make something up, if you did know, what would it be?",
-            "- I am doing the work of three people. I want three more people hired. <br>~Yes, I understand you are working hard. And we both know that HR isn't going to hire three more people. So the question remains, what do you want?",
-            "We recommend that you strengthen your ability to keep commitments by developing your task management skills.",
-            "1) Learn how to set a goal and make a plan for action<br><br>2) How to prioritize activities to maximize time<br><br>3) How to delegate so you are not doing it all by yourself<br><br>4) How to track your time so that you can make accurate time estimates and prevent your time from getting lost in interruptions and unexpected crises",
-        ]
-    }
-]
+  {
+    title: "365 Part 1",
+    subtitle: "365",
+    testdate: "29/09/2022",
+    words: [
+      "A better understanding of culture and humanity through out the world",
+      "We can change our lives. We can do, have, and be exactly what we wish.",
+      "You will not know how far you can go",
+      "Our bodies are our gardens.<br>Our wills are gardeners. <br> -- William Sharespeare",
+      "Fear comes from the selfish idea of cutting one's self off from the universe",
+      "Live your own life. That is to say, where you are, as you are, with what you are, and with who you are. Accept the situation in which you find yourself and try, at the same time, to adapt to it. You cannot escape from it.",
+      "It is only when we give complete attention to a problem, and solve it immediately - never carrying it over to the next day, the next minute, that there is solitude.",
+      "A mind that is burdened with the past is a sorrowful mind.",
+      "The known are our house, our furniture, our family, our character, our work, our knowledge, our fame, our loneliness, our gods...",
+      "There is no master, there is no instructor, there is no person to tell you what you must do.",
+      "When you use the word 'how', you really want someone to tell you what to do, some guide, some system, someone to lead you by the hand so that you lose your freedom, your capacity to observe, your own activities, your own thoughts, your own way of life.",
+      "You cannot expect anything, because the expectation is within you, and what you are waiting for is dependent on external forces.",
+      "We should never try to follow another's path for that is his way, not yours. Your way is the best for you, but that is no sign it is the best for another.",
+      "We do not have the right to force others to see the truth in our way."
+    ]
+  },
+  {
+    title: "365 Part 2",
+    subtitle: "365",
+    testdate: "23/10/2022",
+    words: [
+      "The person who can wear the mantel of a Master is one who is capable of solving his own emotional and mental problems and who can show others the way to overcome their emotional and mental problems.",
+      "To go from opinion to perception, from imagination to fact, from illusion to reality, from something that is not there, to something that is; that is the way forward.",
+      "To grow is to go beyond what you are today. Stand up at yourself. Do not imitate. Do not pretend to have achieved your goal and do not try to cut corner. Just try to grow.",
+      "The human soul travels from law to love, from discipline to freedom, from the moral plane to the spiritual plane.",
+      "Wise people, who know and understand the soul, are indifferent to both pleasure and pain; they have risen above sensations. They are indifferent to the past and the future; they have risen above time. They are indifferent to danger; they have risen above fear. Wise people know that what is here, is also there; that what was, will also be. They see unity, not division.",
+      "The outward freedom that we shall attain will only be in exact proportion to the inward freedom to which we may have grown at any given moment.",
+      "Very few people in this world can reason normally. There is a terrible tendency to accept all that is said, all that is read, and to accept it without question. Only he who is ready to question, to think for himself, will find the truth! <br>To understand the current of a river, he who wishes to know the truth must enter the river.",
+      "By the term religion, I am using it in its broadest sense, meaning thereby self realization or knowledge of the self.",
+      "Allow your son to say foolish things. They are foolish things from your point of view, but when he speaks, agree with him. Say, 'all right, yes. I understand what you mean. Yes, I agree.' Once he has expressed his opinion, then try to show him that there are inconsistencies in some of what he is saying.",
+      "Guard your tongue, for it is highly dangerous; unguarded words can cause terrible distress. A single bad word can destroy a vast quantity of good. A wound caused by fire will eventually heal; but a wound caused by the tongue leaves a scar that never heals.",
+      "Watching and listening are a great art. By watching and listening we learn infinitely more than we do from any books. Books are necessary, but watching and listening sharpen your senses.",
+      "The healing of the mind takes place gradually on contact with nature, with the orange on the branch, the blade of grass eating its way into the cement, and the hills hidden by the clouds."
+    ]
+  },
+  {
+    title: "365 Part 3",
+    subtitle: "365",
+    testdate: "29/10/2022",
+    words: [
+      "How can we be free to look and learn when our minds from the moment we are born to the moment we die are shaped by a particular culture in the narrow pattern of 'me?'",
+      "For centuries, we have been conditioned by nationality, castle, class, tradition, religion, language, education, literature, art, custom, convention, propaganda of all kinds, economic pressure, the food we eat, the climate we live in, our family, our friends, our experience - every influence you can think of - and therefore our responses to every problem are conditioned.",
+      "Are you aware that you are conditioned?",
+      "Sensibility is the capacity to feel, recognize, and distinguish the most tiny and subtle changes.",
+      "No matter how insignificant the thing you have to do, do it as well as you can, give it as much of your care and attention as you would give to the thing you regard as most important.",
+      "The intellectual aspect is, that love sees and understands. The emotional aspect is to feel as one with the other person. Love is unity. There is no 'me' in love, only 'you'. The behavioral aspect is, that love inspires us to give. There is no expectation; we do not expect to receive. Such love is wisdom and liberation in itself."
+    ]
+  },
+  {
+    title: "Management Part 1",
+    subtitle: "Management",
+    testdate: "27/10/2022",
+    words: [
+      "So the best you can do with someone who is constantly complaining, and for everyone around him, is work with him to diminish his feelings of helplessness by helping him to identify solutions.",
+      "a) Listen for main points <br>b) Interrupt and get specific <br>c) Shift the focus to solutions",
+      "- What do you want? <br>~ I don't know <br>- Guess, make something up, if you did know, what would it be?",
+      "- I am doing the work of three people. I want three more people hired. <br>~Yes, I understand you are working hard. And we both know that HR isn't going to hire three more people. So the question remains, what do you want?",
+      "We recommend that you strengthen your ability to keep commitments by developing your task management skills.",
+      "1) Learn how to set a goal and make a plan for action<br><br>2) How to prioritize activities to maximize time<br><br>3) How to delegate so you are not doing it all by yourself<br><br>4) How to track your time so that you can make accurate time estimates and prevent your time from getting lost in interruptions and unexpected crises"
+    ]
+  }
+];
